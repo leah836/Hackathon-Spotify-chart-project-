5e5a5ee33c129775cbdc70d6f218e520d23061eb
leah836 <lstacy26@students.stab.org>
2026-01-07 12:35:32 -0500

    Update minigame.js

--- minigame.js
+++ minigame.js
@@ -1,41 +1,45 @@
-let songs = [];
-let pair = [];
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Spotify%20Charts/Top%2050%20USA.csv"
 
-fetch("https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Spotify%20Charts/Top%2050%20USA.csv")
-  .then(res => res.text())
-  .then(data => {
-    const rows = data.split("\n").slice(1);
+var songs = getColumn(url,2);
+var artist = getColumn(url,3);
+var popularities = getColumn(url,4);
 
-    songs = rows.map(row => {
-      const cols = row.split(",");
-      return {
+var pair;
+var popularity;
 
-        artist: cols[2],
-        popularity: Number(cols[3])
-      };
-    });
+console.log(songs);
+console.log(artist);
 
-    newRound();
-  });
-
+newRound();
 function newRound() {
   pair = [];
-
+  popularity = [];
+  art = [];
   while (pair.length < 2) {
-    const song = songs[Math.floor(Math.random() * songs.length)];
-    if (!pair.includes(song)) pair.push(song);
+    var index = Math.floor(Math.random() * songs.length);
+    console.log(index);
+    const song = songs[index];
+    const pop = popularities[index];
+    const aname = artist[index];
+    if (!pair.includes(song)) {
+      pair.push(song);
+      popularity.push(pop);
+      art.push(aname);
+    }
   }
 
   document.getElementById("song1Btn").textContent =
-pair[0].artist;
+pair[0] + " "+"by" + " " + art[0];
 
   document.getElementById("song2Btn").textContent =
-pair[1].artist;
+pair[1]+" "+"by"+" "+art[1];
 }
 
 function guess(choice) {
-  const correct =
-    pair[0].popularity > pair[1].popularity ? 1 : 2;
+  var correct = 0; 
+  if (popularity[0] < popularity[1]){
+    correct=1
+  }
 
   if (choice === correct) {
     window.location.href = "correct.html";
@@ -43,3 +47,24 @@ function guess(choice) {
     window.location.href = "wrong.html";
   }
 }
+
+//function for the button that leads the the find by rankings page
+function rankings(){
+    location.replace("rankings.html");
+}
+
+//function for the button that leads the the find by artist page
+function findByArtist(){
+    location.replace("artist.html");
+}
+
+//function for the button that leads back to the minigame.html from the wrong.html
+function tryAgain(){
+  location.replace("minigame.html");
+}
+
+////function for the button that leads back to the minigame.html from the correct.html
+function playAgain(){
+  location.replace("minigame.html");
+}
+
