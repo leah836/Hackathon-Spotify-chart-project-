57da167dcc848609d021da8f90b7dedb772896c8
leah836 <lstacy26@students.stab.org>
2026-01-07 12:36:48 -0500

    Update rankingsScript.js

--- rankingsScript.js
+++ rankingsScript.js
@@ -3,14 +3,22 @@ var rank = getColumn(url,1);
 var song = getColumn(url,2);
 var artist = getColumn(url,3)
 
+//function used for the clear button 
 function clearButton(){
     document.getElementById("outputBox").innerHTML = " ";
 }
 
+//function for the button that leads the the find by artist page
 function findByArtist(){
     location.replace("artist.html");
 }
 
+//function for the button that leads to the mini game 
+function game(){
+    location.replace("minigame.html");
+}
+
+//creates the drop down for all of the ranks 1-49
 var dropdown = document.getElementById("rank");
 for(var i = 0; i < rank.length; i++){
     var option = document.createElement("option");
@@ -18,9 +26,9 @@ for(var i = 0; i < rank.length; i++){
     dropdown.appendChild(option);
 }
 
-function getResultsB(){
-var selectedRank = document.getElementById("rank").value;
-console.log(selectedRank);
+/*function that loops through the rank list to find the match to the input
+and makes the output box = the artist and song of that same index*/
+function getResultsB(selectedRank){
 
 document.getElementById("outputBox").innerHTML = " "
 
